9d235e66d04f2a488d1834d865b03c93ec0117a3
jinseong <dlwlstjd1205@gmail.com>
2022-05-06 16:58:24 +0900

    백준 11659 구간 합 구하기 4

---
+++ C study/11659.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int n, m, num1, num2;
+	int i = 0, k = 0, temp[100001] = { 0 };
+	int arr_n[100001] = { 0 };
+
+	scanf("%d %d", &n, &m);
+
+	for (i = 1; i < n+1; i++) {
+		scanf("%d", &arr_n[i]);
+		temp[i] += temp[i - 1] + arr_n[i];
+	}
+
+	for (k = 0; k < m; k++) {
+		int result = 0;
+		scanf("%d %d", &num1, &num2);
+		result = temp[num2] - temp[num1 - 1];
+		printf("%d\n", result);
+	}
+	return 0;
+}
